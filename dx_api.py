48c39f43d15d63761c31aa3b9a8ef9dac21489b5
Maga <maga@LAPTOP-1I6JJP9Q>
2023-10-22 07:07:02 +0300

    Добвлены программы на питон для получения данных из DxSale и разных blockchain explorer'ов

---
+++ dx_api.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+# URL эндпоинта API
+url = "https://scan.dx.app/api/v2/sales/offChain/successfulSales"
+page = 1
+# Параметры запроса, если необходимо
+params = {"page": page, 'limit': 1000}
+
+# Выполнение GET-запроса
+response = requests.get(url, params=params)
+
+# Проверка успешности запроса
+if response.status_code == 200:
+    data = response.json()  # Извлечение данных из JSON-ответа
+else:
+    print(f"Ошибка {response.status_code}: {response.text}")
+
+totalItems = data['meta']['totalItems']
+print(totalItems)
+print(data['items'][1])
+
+with open("dx_data.json", "w") as file:
+    json.dump(data, file)
