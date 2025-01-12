ee39215e16fca337fc7844ca7c4c7a1a42d3e317
Магомед <ma9ickise@icloud.com>
2025-01-12 11:02:29 +0300

    add relevant parsers

---
+++ parsers/getcurrenciesdata.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import json
+import time
+import subprocess
+import sys
+
+# собрать данные (id, name, contract_addresses) имея список криптовалют
+
+platforms_url = "https://cryptorank.io/fundraising-platforms"
+platforms_class = 'sc-d76e30e5-1 gyiaYN'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'X-api-key': 'fb22f1774649a04e17f33d1cb45fc8f083eae84c2ebed5c77447aaf6424d'
+    # Другие заголовки по необходимости
+}
+
+def extract_crypto_currency_data(currence_id):
+
+    response = requests.get(f"https://api.cryptorank.io/v2/currencies/{currence_id}", headers=headers)
+    if response.status_code == 200:
+    # Получаем HTML-код из содержимого ответа
+        data = response.json()
+        print(data)
+        return data
+    else:
+        print(f"Ошибка при запросе: {response.status_code}")
+        time.sleep(5)
+
+data = dict()
+with open("../data/crypto_list.json", "r") as file:
+    data = json.load(file)
+
+
+with open("../data/currency_data.json", "r") as file:
+    currency_data_dict = json.load(file)
+
+start_i = 12
+end_i = 411
+
+currency_data = currency_data_dict['items']
+for i in range(start_i, end_i):
+    item = data['data'][i]
+    time.sleep(1)
+    currency_data.append({'lifeCycle': item['lifeCycle'], 'currency_data': extract_crypto_currency_data(item['id'])})
+    print(len(currency_data))
+
+currency_data_dict = {'items' : currency_data}
+with open("../data/currency_data.json", "w") as file:
+    json.dump(currency_data_dict, file)
---
+++ parsers/getjsonwithsourcecode.py
@@ -0,0 +1,70 @@
+import requests
+import json
+
+# Сбор кодов криптовалют по их имеющимся данным
+
+def get_api_info(apis_dict, name):
+    url = None
+    apikey = None
+    for api in apis_dict["apis"]:
+        if api["scan_name"] == name:
+            url = api["url"]
+            apikey = api["apikey"]
+    return (url, apikey)
+
+
+def valid_info(url, apikey):
+    return url is not None and apikey is not None
+
+def data_from_json(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+def get_chain_name_from_item(item):
+    try:
+        return item['currency_data']['data']['contracts'][0]['platform']['name']
+    except:
+        return False
+
+def get_address_from_item(item):
+    return item['currency_data']['data']['contracts'][0]['address']
+
+def get_token_name_from_item(item):
+    return item['currency_data']['data']['name']
+
+def get_life_cycle_from_item(item):
+    return item['lifeCycle']
+
+data = dict()
+with open("../data/currency_data.json", "r") as file:
+    data = json.load(file)
+apis_dict = data_from_json("apis_dict.json")
+
+
+res_data = {'items':[]}
+
+for item in data["items"]:
+
+    name = get_chain_name_from_item(item)
+    if not name:
+        continue
+    address = get_address_from_item(item)
+    token_name = get_token_name_from_item(item)
+    life_cycle = get_life_cycle_from_item(item)
+
+    url, apikey = get_api_info(apis_dict, name)
+    if valid_info(url, apikey):
+
+        # Выполнение GET-запроса
+        params = {"module": "contract", "action": "getsourcecode", "address": address, "apikey": apikey}
+        response = requests.get(url, params=params)
+
+        # Проверка успешности запроса
+        if response.status_code == 200:
+            res_data['items'].append({'name': token_name, 'lifeCycle': life_cycle,
+                              'response': response.json()})  # Извлечение данных из JSON-ответа
+        else:
+            print(f"Ошибка {response.status_code}: {response.text}")
+
+with open("../data/cryptorank_data.json", "w") as file:
+    json.dump(res_data, file)
