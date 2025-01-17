6a1f154d7c469bd58d7c1ed37c50e96a743f9ec2
Магомед <ma9ickise@icloud.com>
2025-01-17 14:16:39 +0300

    refactor code and add dropdown menu

--- service/main.py
+++ service/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from predictor import ScamPredictor
 from pydantic import BaseModel
 from cryptoApi import get_source_code
+from typing import Literal
 import requests
 
 from fastapi_cache import FastAPICache
@@ -10,21 +11,23 @@ from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
 
 
+base_address = "0x9f589e3eabe42ebc94a44727b3f3531c0c877809"
+base_platform = 'BNB'
+
 class CodeItem(BaseModel):
     code: str
 
 
 class FindItem(BaseModel):
-    platform: str
-    address: str
+    platform: Literal['BNB', 'Ethereum', 'Fantom', 'Polygon', 'Base', 'Arbitrum', 'Core'] = 'BNB'
+    address: str = "0x9f589e3eabe42ebc94a44727b3f3531c0c877809"
 
 
 app = FastAPI()
 model = ScamPredictor()
 
 
-base_address = "0x9f589e3eabe42ebc94a44727b3f3531c0c877809"
-base_platform = 'BNB'
+
 
 @app.on_event("startup")
 async def startup():
@@ -41,8 +44,7 @@ async def root():
 @app.post("/code_predict/")
 # @cache(expire=30)
 async def predict_by_code(item: CodeItem):
-    """predict the class and probability of not scam crypto project
-    by contract code of token"""
+    """predict the activity of crypto project by its code"""
 
     predicted_label = model.predict(item.code)
 
@@ -51,37 +53,24 @@ async def predict_by_code(item: CodeItem):
 
     return {"message": f"Данная криптовалюта инактивна"}
 
-@app.post("/code_predict_label/")
-# @cache(expire=30)
-async def predict_label_by_code(item: CodeItem):
-    """predict the class and probability of not scam crypto project
-    by contract code of token"""
 
-    predicted_label = model.predict(item.code)
-
-    return {"message": f"{predicted_label}"}
-
-
-
-@app.get("/predict/")
+@app.post("/predict/")
 @cache(expire=30)
-async def predict_by_address_and_platform(address: str = base_address, platform: str = base_platform):
-    """predict the class and probability of not scam crypto project
-    by platform and contract address of token"""
-    code = get_source_code(address, platform)
+async def predict_by_address_and_platform(item: FindItem = Depends()):
+    """predict the activity of crypto project based on machine learning model"""
+    code = get_source_code(item.address, item.platform)
     predicted_label = model.predict(code)
 
     if predicted_label:
         return {"message": f"Данная криптовалюта будет активна"}
 
-    return {"message": f"Данная криптовалюта инактивна"}
+    return {"message": f"Данная криптовалюта будет инактивна"}
 
-
-@app.get("/code/")
+@app.post("/code/")
 @cache(expire=30)
-async def get_code_by_address_and_platform(address: str = base_address, platform: str = base_platform):
+async def get_code_by_address_and_platform(item: FindItem = Depends()):
     """requests the contract code by platform and contract address of token"""
-    code = get_source_code(address, platform)
+    code = get_source_code(item.address, item.platform)
     return {"code": f"{code}"}
 
 
